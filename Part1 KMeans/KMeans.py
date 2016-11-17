1320d20ad4cd3f03587e571147df78c5aa6c3cc9
Dvir Segal <dvirsegal84@gmail.com>
2016-11-17 17:42:20 +0200

    Second Revision

--- Part1 KMeans/KMeans.py
+++ Part1 KMeans/KMeans.py
@@ -1,7 +1,36 @@
 # Import the pandas library.
 import pandas
+# Import matplotlib
+import matplotlib.pyplot as plt
+# Import the kmeans clustering model.
+from sklearn.cluster import KMeans
+# Import the PCA model.
+from sklearn.decomposition import PCA
 
 # Read in the data.
 games = pandas.read_csv("games.csv")
-# Print the names of the columns in games.
-print(games.columns)
+
+# Remove any rows without user reviews.
+games = games[games["users_rated"] > 0]
+# Remove any rows with missing values.
+games = games.dropna(axis=0)
+
+# Initialize the model with 2 parameters -- number of clusters and random state.
+kmeans_model = KMeans(n_clusters=5, random_state=10)
+# Get only the numeric columns from games.
+good_columns = games._get_numeric_data()
+# Fit the model using the good columns.
+kmeans_model.fit(good_columns)
+# Get the cluster assignments.
+labels = kmeans_model.labels_
+
+# Create a PCA model.
+pca_2 = PCA(2)
+# Fit the PCA model on the numeric columns from earlier.
+plot_columns = pca_2.fit_transform(good_columns)
+# Make a scatter plot of each game, shaded according to cluster assignment.
+plt.scatter(x=plot_columns[:,0], y=plot_columns[:,1], c=labels)
+# Show the plot.
+plt.show()
+
+
