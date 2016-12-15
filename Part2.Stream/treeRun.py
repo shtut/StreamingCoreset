4f09868a90aa3221b8f6e39fb48fb9960085c836
RoyGoldstein <roy.z.goldstein@gmail.com>
2016-12-15 13:45:12 +0200

    CoresetTreeBuilder interface was updated

--- Part2.Stream/treeRun.py
+++ Part2.Stream/treeRun.py
@@ -3,7 +3,7 @@ from stack import Stack
 from CoresetTreeBuilder import CoresetTreeBuilder
 import  numpy as np
 
-s = CoresetTreeBuilder(simpleCoreset.CoreSetHandler.coreset_alg, 20, 4)
+s = CoresetTreeBuilder(simpleCoreset.CoreSetHandler.coreset_alg, 20)
 s.add_points(np.random.rand(1000,100))
 res = s.get_unified_coreset()
 print "####################################################"
