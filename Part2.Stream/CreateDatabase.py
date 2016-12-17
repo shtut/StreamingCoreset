48a0f69a003770093f8c8dec8b87d5bbbfd86d7e
Dvir Segal <dvirsegal84@gmail.com>
2016-12-17 12:18:44 +0200

    Create DB: Ability to read chunk by chunk
    1. refactor current method to more generaly manner
    2. read csv DB by chunk size and "do something"

--- Part2.Stream/CreateDatabase.py
+++ Part2.Stream/CreateDatabase.py
@@ -3,27 +3,58 @@ import pandas as pd
 import scipy.sparse as sps
 
 
+def process_chunk(chuck):
+    print len(chuck)
+    # do something useful ...
+
+
+# noinspection PyMethodMayBeStatic
 class createDB:
     """
     This class purpose is to manipulate database in CSV format
     The constructor generates 2 sparse matrices and zip between them
     """
-    def __init__(self, rows, cols):
+
+    def __init__(self, method_to_run):
+        self._processChunkMethod = method_to_run
+        pass
+
+    def create_matrix(self, rows, cols):
         """
-        The constructor generates 2 sparse matrices and zip between them
+        Generates 2 sparse matrices and zip between them
         :param rows: given number of rows
         :param cols: given number of columns
         """
-        a = sps.rand(rows, cols, density=0.10, format='csr')
-        a.data[:] = 1
-        a = a.toarray()
-        b = sps.random(rows, cols, density=0.25, format='csr')
-        b.data[:] = 1
-        b = b.toarray()
-        c = []
-        for elem in zip(a, b):
-            c.extend(elem)
-        self._db = c
+        first_sparse = self.generate_uniform_sparse_matrix(rows, cols, 0.10)
+        second_sparse = self.generate_random_sparse_matrix(rows, cols, 0.25)
+        merged_sparse = []
+        for elem in zip(first_sparse, second_sparse):
+            merged_sparse.extend(elem)
+        self._db = merged_sparse
+
+    def generate_random_sparse_matrix(self, rows, cols, density):
+        """
+        Generate a sparse matrix with random distribution
+        :param cols: number of columns in generated matrix
+        :param rows: number of rows in generated matrix
+        :param density: the density of values in the sparse matrix
+        :return: the sparse matrix
+        """
+        temp = sps.random(rows, cols, density, format='csr')
+        temp.data[:] = 1
+        return temp.toarray()
+
+    def generate_uniform_sparse_matrix(self, rows, cols, density):
+        """
+        Generate a sparse matrix with uniform distribution
+        :param cols: number of columns in generated matrix
+        :param rows: number of rows in generated matrix
+        :param density: the density of values in the sparse matrix
+        :return: the sparse matrix
+        """
+        temp = sps.rand(rows, cols, density, format='csr')
+        temp.data[:] = 1
+        return temp.toarray()
 
     @property
     def db(self):
@@ -33,8 +64,7 @@ class createDB:
         """
         return self._db
 
-    @staticmethod
-    def write_matrix_to_csv(file_name, array):
+    def write_matrix_to_csv(self, file_name, array):
         """
         The purpose of this method is to write a given array into CSV file
         :param file_name: given file name to write into
@@ -45,17 +75,23 @@ class createDB:
             for row in array:
                 writer.writerow(row)
 
-    @staticmethod
-    def read_from_csv(file_name):
+    def read_from_csv(self, file_name, chunksize):
         """
         The purpose of this method is to read from a given file and create an array from it
+        The method reads chunk by chunk from a CSV file and process it
+        :param chunksize: given size of chunk to read from CSV
         :param file_name: given file name to read from
-        :return: the loaded array
         """
-        df = pd.read_csv(file_name, sep=',', header=None)
-        return df.values
+        reader = csv.reader(open(file_name, 'rb'))
+        chunk = []
+        for i, line in enumerate(reader):
+            if i % chunksize == 0 and i > 0:
+                self._processChunkMethod(chunk)
+                del chunk[:]
+            chunk.append(line)
 
 
-# c = createDB(1000000, 10)
-# createDB.write_matrix_to_csv('1.csv', c.db)
-a = createDB.read_from_csv('1.csv')
+c = createDB(process_chunk)
+c.create_matrix(1000000, 10)
+c.write_matrix_to_csv('1.csv', c.db)
+c.read_from_csv('1.csv', 100)
