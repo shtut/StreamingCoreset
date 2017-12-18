b38db951187c6c06739a8a4a5619e39199a25f29
Dvir Segal <dvirsegal84@gmail.com>
2017-12-18 21:45:27 +0200

    Fix runner to use threads
    1. Runner with threads
    2. Add batch files to easly run env.

--- StremingCoreset/coreset_finder/worker_runner.py
+++ StremingCoreset/coreset_finder/worker_runner.py
@@ -2,13 +2,24 @@
 Creates and runs a worker.
 """
 from worker import Worker
+from threading import Thread
+import configuration as cfg
+import time
 import sys
 
 #starts up the worker
 try:
     if __name__ == '__main__':
-        worker = Worker(sys.argv[1] , sys.argv[2])
-        worker.register_and_handle()
+
+        # for different processes
+        # worker = Worker(sys.argv[1] , sys.argv[2])
+        # worker.register_and_handle()
+
+        # for the same PC with threads
+        for i in xrange(cfg.NUMBER_OF_WORKERS):
+            worker = Worker(sys.argv[1], cfg.LEAF_SIZE)
+            Thread(target=worker.register_and_handle).start()
+            time.sleep(1)
 except KeyboardInterrupt:
     print "caught SIGINT, dying."
     exit()
